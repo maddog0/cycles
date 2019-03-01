bba0f5518b047c5ac6001effce583313ec6941f2
William Callanan <willbob9@gmail.com>
2019-03-01 12:24:31 -0500

    content
    adding all prexisting files

---
+++ Assets/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relativePos = (target.position + new Vector3(.25f, 0, 0)) - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+        Quaternion current = transform.localRotation;
+
+        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
+        transform.Translate(0, 0, 10 * Time.deltaTime);
+    }
+}
