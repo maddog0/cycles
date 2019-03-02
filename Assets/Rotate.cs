81292e48dd6a3c4097ab71fb98a5f74c8912dd94
William Callanan <willbob9@gmail.com>
2019-03-02 18:07:11 -0500

    code move
    Moved trajectory line code to own script.  Turned off trajectory line because it isn't working properly

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -7,14 +7,11 @@ public class Rotate : MonoBehaviour
     public Transform target;
     public int speed;
     public float distance;
-    float initialDistanceVelocity = -100.0f;
-    float finalDistanceVelocity = 100.0f;
+    public float initialDistanceVelocity = -100.0f;
+    public float finalDistanceVelocity = 100.0f;
     public float currentDistanceVelocity = 0.0f;
     float distanceAccelerationRate = 0.01f;
-    float distanceDecelerationRate = 0.001f;
-    int iteration = 0;
-    public int trajLength = 50;
-    public LineRenderer trajectory;
+    public float distanceDecelerationRate = 0.001f;
 
     // Update is called once per frame
     void Update()
@@ -47,29 +44,6 @@ public class Rotate : MonoBehaviour
 
         transform.Translate(0, 0, speed*Time.deltaTime);
 
-        //Trajectory line function
-        trajectory.positionCount = 1;
-        trajectory.SetPosition(0, transform.position);
-        iteration = 1;
-        float cDVtemp = currentDistanceVelocity;
-        float distancetemp = distance;
-        Vector3 rPtemp = transform.position;
-        Quaternion ctemp = transform.localRotation;
-        while (iteration < trajLength)
-        {
-            cDVtemp -= distanceDecelerationRate * Time.deltaTime;
-            cDVtemp = Mathf.Clamp(cDVtemp, initialDistanceVelocity, finalDistanceVelocity);
-            distancetemp += cDVtemp;
-            if (distancetemp < 0)
-            {
-                distancetemp = 0;
-            }
-            Quaternion rotationtemp = Quaternion.LookRotation(rPtemp, Vector3.forward);
-            ctemp = Quaternion.Slerp(ctemp, rotationtemp, (Time.deltaTime / (distance + 40)) * speed * 20);
-            rPtemp += (ctemp * Vector3.back) * speed * Time.deltaTime;
-            trajectory.positionCount = iteration + 1;
-            trajectory.SetPosition(iteration, rPtemp);
-            iteration += 1;
-        }
+        
     }
 }
---
+++ Assets/Trajectory.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trajectory : MonoBehaviour
+{
+    int iteration = 0;
+    public int trajLength = 5;
+    public LineRenderer trajectory;
+    public Rotate rotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Trajectory line function
+        trajectory.positionCount = 1;
+        trajectory.SetPosition(0, transform.position);
+        iteration = 1;
+        float cDVtemp = rotate.currentDistanceVelocity;
+        float distancetemp = rotate.distance;
+        Vector3 rPtemp = transform.position;
+        Quaternion ctemp = transform.localRotation;
+        while (iteration < trajLength)
+        {
+            cDVtemp -= rotate.distanceDecelerationRate * Time.deltaTime;
+            cDVtemp = Mathf.Clamp(cDVtemp, rotate.initialDistanceVelocity, rotate.finalDistanceVelocity);
+            distancetemp += cDVtemp;
+            if (distancetemp < 0)
+            {
+                distancetemp = 0;
+            }
+            Quaternion rotationtemp = Quaternion.LookRotation(rPtemp, Vector3.forward);
+            ctemp = Quaternion.Slerp(ctemp, rotationtemp, (Time.deltaTime / (rotate.distance + 40)) * rotate.speed * 20);
+            rPtemp += (ctemp * Vector3.back) * rotate.speed * Time.deltaTime;
+            trajectory.positionCount = iteration + 1;
+            trajectory.SetPosition(iteration, rPtemp);
+            iteration += 1;
+        }
+    }
+}
